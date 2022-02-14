e48f683095830e056e8d91232abbc04d206f260f
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-02-15 01:37:58 +0300

    feat: add functions show images, create images

---
+++ image-galery/index.js
@@ -0,0 +1,30 @@
+const apiKey = 'UxfSEhDor1r1v1sLqoxWvXTZJ7VwuhQUX1VyBCUjg_g';
+const url = `https://api.unsplash.com/search/photos?query=men&client_id=${apiKey}`;
+
+
+async function getData() {
+  const res = await fetch(url);
+  const resData = await res.json();
+  console.log(resData);
+
+  showDate(resData)
+}
+getData(url);
+
+/*------------SHOW IMAGES---------------------------------------*/
+
+function showDate(data) {
+  data.results.map(e => createImages(e));
+}
+
+/*------------CREATE IMAGES-------------------------------------*/
+
+function createImages(elem) {
+  const mainGrid = document.querySelector('.main__grid');
+  const img = document.createElement('img');
+
+  img.classList.add('main-img')
+  img.src = `${elem.urls.regular}`;
+  img.alt = `image`;
+  mainGrid.append(img);
+}
