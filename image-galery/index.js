440bce8753492f23b4965f48b90d1862aadeebbf
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-02-15 07:50:41 +0300

    feat: add event listener form

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,30 +1,59 @@
 const apiKey = 'UxfSEhDor1r1v1sLqoxWvXTZJ7VwuhQUX1VyBCUjg_g';
-const url = `https://api.unsplash.com/search/photos?query=js&per_page=30&orientation=landscape&client_id=${apiKey}`;
+const urlDef = `https://api.unsplash.com/search/photos?per_page=30&orientation=landscape&client_id=${apiKey}&query=js`;
+const urlSearch = `https://api.unsplash.com/search/photos?per_page=30&orientation=landscape&client_id=${apiKey}&query=`;
 
 
-async function getData() {
+async function getData(url) {
   const res = await fetch(url);
   const resData = await res.json();
-  console.log(resData);
 
   showDate(resData)
 }
-getData(url);
+
+getData(urlDef);
 
 /*------------SHOW IMAGES---------------------------------------*/
 
 function showDate(data) {
-  data.results.map(e => createImages(e));
+  document.querySelector('.main__grid').innerHTML = '';
+  data.results.map(item => createImages(item));
 }
 
 /*------------CREATE IMAGES-------------------------------------*/
 
 function createImages(elem) {
   const mainGrid = document.querySelector('.main__grid');
-  const img = document.createElement('img');
+  const div = document.createElement('div');
+  const text = document.createElement('p')
 
-  img.classList.add('main-img')
-  img.src = `${elem.urls.regular}`;
-  img.alt = `image`;
-  mainGrid.append(img);
+  div.classList.add('main-img')
+  div.style.backgroundImage = `url(${elem.urls.regular})`;
+
+  text.classList.add('main-text')
+  text.innerHTML = `Photo author: ${elem.user.name}`;
+
+  div.appendChild(text);
+  mainGrid.appendChild(div);
 }
+
+/*------------EVENT LISTENER------------------------------------*/
+
+const form = document.querySelector('form');
+const searchInp = document.querySelector('.search__inp');
+const searchBtn = document.querySelector('.search__btn');
+
+form.addEventListener('submit', item => {
+  const UrlApiSearch = `${urlSearch}${searchInp.value}`;
+
+  if (searchInp.value) {
+    getData(UrlApiSearch);
+  }
+})
+
+searchBtn.addEventListener('clic', item => {
+  const UrlApiSearch = `${urlSearch}${searchInp.value}`;
+
+  if (searchInp.value) {
+    getData(UrlApiSearch);
+  }
+})
